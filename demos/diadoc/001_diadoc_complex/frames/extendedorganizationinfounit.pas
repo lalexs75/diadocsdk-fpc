c0b33c6e81c6708ce515f6fe6ff00b83a79c66f6
lalexs75 <alexs75@yandex.ru>
2020-02-05 11:10:17 +0300

    Замечание о лицензии на пример работы

--- demos/diadoc/001_diadoc_complex/frames/extendedorganizationinfounit.pas
+++ demos/diadoc/001_diadoc_complex/frames/extendedorganizationinfounit.pas
@@ -1,3 +1,28 @@
+{ Комплексный пример работы с ДИАДОК
+
+  Copyright (C) 2018-2020 Lagunov Aleksey alexs75@yandex.ru
+
+  Этот исходный код является свободно-распространяемым программынм обеспечением.
+  Вы можете распространять и (или) модифицировать его при условии соблюдения
+  условий лицензии GPL2, опублиованной Фондом свободного програмного обеспечения
+  (FSF).
+
+  This source is free software; you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free
+  Software Foundation; either version 2 of the License, or (at your option)
+  any later version.
+
+  This code is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+  details.
+
+  A copy of the GNU General Public License is available on the World Wide Web
+  at <http://www.gnu.org/copyleft/gpl.html>. You can also obtain it by writing
+  to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+  MA 02111-1307, USA.
+}
+
 unit ExtendedOrganizationInfoUnit;
 
 {$mode objfpc}{$H+}
