146e4e588a18b51e30b3f48c044d87a735e8bb18
alexs <alexs75@yandex.ru>
2020-04-20 22:02:08 +0300

    Дополнительные функции и документация

--- DiadocVisual/ddconsts.pas
+++ DiadocVisual/ddconsts.pas
@@ -35,12 +35,90 @@ unit ddConsts;
 interface
 
 uses
-  Classes, SysUtils;
+  Classes, SysUtils, DiadocTypes;
 
 resourcestring
   sSelectClientCaption = 'Найденные контрагенты';
+  sdfsAny = 'любой документ';
+  sdfsNotRead = 'документ не прочитан';
+  sdfsNoRecipientSignatureRequest = 'документ без запроса ответной подписи';
+  sdfsWaitingForRecipientSignature = 'документ в ожидании ответной подписи';
+  sdfsWithRecipientSignature = 'документ с ответной подписью';
+  sdfsWithSenderSignature = 'документ с подписью отправителя';
+  sdfsRecipientSignatureRequestRejected = 'документ с отказом от формирования ответной подписи';
+  sdfsWaitingForSenderSignature = 'документ, требующий подписания и отправки';
+  sdfsInvalidSenderSignature = 'документ с невалидной подписью отправителя, требующий повторного подписания и отправки';
+  sdfsInvalidRecipientSignature = 'документ с невалидной подписью получателя, требующий повторного подписания и отправки';
+  sdfsApproved = 'согласованный документ';
+  sdfsDisapproved = 'документ с отказом согласования';
+  sdfsWaitingForResolution = 'документ, находящийся на согласовании или подписи';
+  sdfsSignatureRequestRejected = 'документ с отказом в запросе подписи сотруднику';
+  sdfsFinished = 'документ с завершенным документооборотом';
+  sdfsHaveToCreateReceipt = 'требуется подписать извещение о получении';
+  sdfsNotFinished = 'документ с незавершенным документооборотом';
+  sdfsInvoiceAmendmentRequested = 'имеет смысл только для счетов-фактур; документ, по которому было запрошено уточнение';
+  sdfsRevocationIsRequestedByMe = 'документ, по которому было запрошено аннулирование';
+  sdfsRequestsMyRevocation = 'документ, по которому контрагент запросил аннулирование';
+  sdfsRevocationAccepted = 'аннулированный документ';
+  sdfsRevocationRejected = 'документ, запрос на аннулирование которого был отклонен';
+  sdfsRevocationApproved = 'документ, запрос на аннулирование которого был согласован';
+  sdfsRevocationDisapproved = 'документ с отказом согласования запроса на аннулирование';
+  sdfsWaitingForRevocationApprovement = 'документ, находящийся на согласовании запроса аннулирования';
+  sdfsNotRevoked = 'неаннулированный документ';
+  sdfsWaitingForProxySignature = 'документ в ожидании подписи промежуточного получателя';
+  sdfsWithProxySignature = 'документ с подписью промежуточного получателя';
+  sdfsInvalidProxySignature = 'документ с невалидной подписью промежуточного получателя, требующий повторного подписания и отправки';
+  sdfsProxySignatureRejected = 'документ с отказом от формирования подписи промежуточным получателем';
+  sdfsWaitingForInvoiceReceipt = 'документ в ожидании получения извещения о получении счета-фактуры';
+  sdfsWaitingForReceipt = 'документ в ожидании получения извещения о получении';
+  sdfsRequestsMySignature = 'документ, по которому контрагент запросил подпись';
+  sdfsRoamingNotificationError = 'документ, с ошибкой доставки в роуминге';
 
+function DocumentFilterStatusText(ADocumentFilterStatus:TDocumentFilterStatus):string;
 implementation
+
+function DocumentFilterStatusText(ADocumentFilterStatus: TDocumentFilterStatus
+  ): string;
+begin
+  case ADocumentFilterStatus of
+    dfsAny: Result:=sdfsAny;
+    dfsNotRead: Result:=sdfsNotRead;
+    dfsNoRecipientSignatureRequest: Result:=sdfsNoRecipientSignatureRequest;
+    dfsWaitingForRecipientSignature: Result:=sdfsWaitingForRecipientSignature;
+    dfsWithRecipientSignature: Result:=sdfsWithRecipientSignature;
+    dfsWithSenderSignature: Result:=sdfsWithSenderSignature;
+    dfsRecipientSignatureRequestRejected: Result:=sdfsRecipientSignatureRequestRejected;
+    dfsWaitingForSenderSignature: Result:=sdfsWaitingForSenderSignature;
+    dfsInvalidSenderSignature: Result:=sdfsInvalidSenderSignature;
+    dfsInvalidRecipientSignature: Result:=sdfsInvalidRecipientSignature;
+    dfsApproved: Result:=sdfsApproved;
+    dfsDisapproved: Result:=sdfsDisapproved;
+    dfsWaitingForResolution: Result:=sdfsWaitingForResolution;
+    dfsSignatureRequestRejected: Result:=sdfsSignatureRequestRejected;
+    dfsFinished: Result:=sdfsFinished;
+    dfsHaveToCreateReceipt: Result:=sdfsHaveToCreateReceipt;
+    dfsNotFinished: Result:=sdfsNotFinished;
+    dfsInvoiceAmendmentRequested: Result:=sdfsInvoiceAmendmentRequested;
+    dfsRevocationIsRequestedByMe: Result:=sdfsRevocationIsRequestedByMe;
+    dfsRequestsMyRevocation: Result:=sdfsRequestsMyRevocation;
+    dfsRevocationAccepted: Result:=sdfsRevocationAccepted;
+    dfsRevocationRejected: Result:=sdfsRevocationRejected;
+    dfsRevocationApproved: Result:=sdfsRevocationApproved;
+    dfsRevocationDisapproved: Result:=sdfsRevocationDisapproved;
+    dfsWaitingForRevocationApprovement: Result:=sdfsWaitingForRevocationApprovement;
+    dfsNotRevoked: Result:=sdfsNotRevoked;
+    dfsWaitingForProxySignature: Result:=sdfsWaitingForProxySignature;
+    dfsWithProxySignature: Result:=sdfsWithProxySignature;
+    dfsInvalidProxySignature: Result:=sdfsInvalidProxySignature;
+    dfsProxySignatureRejected: Result:=sdfsProxySignatureRejected;
+    dfsWaitingForInvoiceReceipt: Result:=sdfsWaitingForInvoiceReceipt;
+    dfsWaitingForReceipt: Result:=sdfsWaitingForReceipt;
+    dfsRequestsMySignature: Result:=sdfsRequestsMySignature;
+    dfsRoamingNotificationError: Result:=sdfsRoamingNotificationError;
+  else
+    Result:='Ошибка!';
+  end;
+end;
 
 end.
 
